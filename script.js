30690f5ea2c6256c9381cba0c0c71d683ca288dc
ahmedasik404 <rabbiahmed454545@gmail.com>
2025-03-31 01:39:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function myFunction() {
+  document.getElementById("nav").style.display = "block";
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("cl").style.display = "block";
+
+};
+
+function myFunctions() {
+    document.getElementById("nav").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("cl").style.display = "none";
+  
+  };
+
+  function myFunctionr() {
+    document.getElementById("invite").style.display="block";
+  }
+
+  function accept() {
+
+    document.getElementById("stxt").style.display="block";
+    console.log("asik");
+  };
+
+  function closer() {
+    console.log("asik");
+    document.getElementById("stxt").style.display="none";
+  }
+
+  function closeo() {
+ document.getElementById("invite").style.display="none";
+  }
